623ebb52e0363374a86d923024aae1a96967f133
Shanza Saleem <s.shanza1995@gmail.com>
2025-11-22 21:41:44 +0000

    Refactor comments to include task numbers
    Updated comments to include task numbers for clarity and organization.

--- Airline Analysis.sql
+++ Airline Analysis.sql
@@ -3,21 +3,24 @@
 create database airline_analysis;
 use airline_analysis;
 
-# WAQ to display all the passengers (customers) who have travelled in routes 01 to 25. 
-		# Take data from the passengers_on_flights table
+## Task 1: WAQ to display all the passengers (customers) who have travelled in routes 01 to 25. 
+
 select * from passengers_on_flights
 where route_id between 01 and 25;
 
-# WAQ to identify the number of passengers and total revenue in business class from the ticket_details table.
+## Task 2: WAQ to identify the number of passengers and total revenue in business class from the ticket_details table.
+	
 select sum(no_of_tickets) as no_of_passengers, sum(no_of_tickets*Price_per_ticket) as total_revenue 
 from ticket_details
 where class_id = 'Business';
 
-# WAQ to display the full name of the customer by extracting the first name and last name from the customer table.
+## Task 3: WAQ to display the full name of the customer by extracting the first name and last name from the customer table.
+	
 select concat(first_name, ' ', last_name) as Full_Name from customer;
 
-# WAQ to extract the customers who have registered and booked a ticket. Use data from customer and ticket_details tables.
-select c.customer_id, c.first_name, c.last_name,
+## Task 4: WAQ to extract the customers who have registered and booked a ticket. Use data from customer and ticket_details tables.
+
+	select c.customer_id, c.first_name, c.last_name,
 		td.aircraft_id, td.a_code, td.p_date
 from 
 	customer c
@@ -25,61 +28,58 @@ join
 	ticket_details td
 on c.customer_id = td.customer_id;
 
-# WAQ to identify the customer's first name and last name based on their customer_id and brand (Emirates) from the ticket_details table
+## Task 5: WAQ to identify the customer's first name and last name based on their customer_id and brand (Emirates) from the ticket_details table.
+'
 select c.first_name, c.last_name, c.customer_id, td.brand
-from customer c 
-join ticket_details td 
-on c.customer_id = td.customer_id
-where td.brand = 'Emirates';
+from 
+	customer c 
+join
+	ticket_details td 
+on 
+	c.customer_id = td.customer_id
+where
+	td.brand = 'Emirates';
 
-# WAQ to identify the customers who have travelled by Economy Plus class 
-		# using Group By and Having clause on the passengers_on_flights table. 
-select customer_id, count(*) as total_flights from passengers_on_flights where class_id ='Economy Plus'
+## Task 6: WAQ to identify the customers who have travelled by Economy Plus class 
+		
+select customer_id, count(*) as total_flights from passengers_on_flights
+where class_id ='Economy Plus'
 group by customer_id
-having count(*)>0;				# count(*)>0 ensures only customers with atleast one matching record are shown
+having count(*)>0;				
 
-# WAQ to identify whether the revenue has crossed 10000 using the IF CLAUSE on the ticket_details table.
+## Task 7: WAQ to identify whether the revenue has crossed 10000 using the IF CLAUSE on the ticket_details table.
+
 select sum(no_of_tickets*Price_per_ticket) as Revenue,
 if(sum(no_of_tickets*Price_per_ticket) > 10000, 'Yes', 'No') as revenue_crossed_10000
 from ticket_details;
 
-# WAQ to create and grant access to a new user to perform operations on a database.
+## Task 8: WAQ to create and grant access to a new user to perform operations on a database.
+
 CREATE USER 'new_user' @ 'localhost' IDENTIFIED BY 'user_password';
 GRANT ALL PRIVILEGES ON database_name. * TO 'new_user' @ 'localhost';
 # In case we want to grant access with Limited Permissions
 GRANT SELECT, INSERT, UPDATE ON database_name. * TO 'new_user' @ 'localhost';
 
-# WAQ to find the maximum ticket price for each class using window functions on the ticket_details table.
+## Task 9: WAQ to find the maximum ticket price for each class using window functions on the ticket_details table.
 
 select customer_id, class_id, Price_per_ticket, 
 MAX(Price_per_ticket) OVER (PARTITION BY customer_id ORDER BY class_id) as max_ticket_price 
 from ticket_details; 
 
-# WAQ to extract the passengers whose route_id is 4 by improving the speed and performance of the passengers_on_flights table. 
+## Task 10: WAQ to extract the passengers whose route_id is 4 by improving the speed and performance of the passengers_on_flights table. 
+
 select * from passengers_on_flights where route_id = 4;
+
 # create index for query speed
 CREATE INDEX idx_route_id ON passengers_on_flights(route_id);
 
-# for the route_id 4, WAQ to view the execution plan of the passengers_on_flights table
+# for the route_id 4, WAQ to view the execution plan of the passengers_on_flights table.
+	
 EXPLAIN select * from passengers_on_flights where route_id = 4; 
 
-# WAQ to calculate the total price of all tickets booked by a customer across different aircraft IDs using rollup function.
-
-select customer_id, aircraft_id, sum(Price_per_ticket) as tot_price_all_tickets
-from ticket_details
-group by customer_id, aircraft_id 
-with rollup ;
-
-/* ROLLUP function is used to generate subtotals and grand totals when performing aggregations with GROUP BY.
-- groups data at multiple level of aggregation
-- adds SUBTOTAL rows for each grouping hierarchy
-- add a GRAND TOTAL row at the end.
-
-In the query we resolved the task but for better readability and understanding the outcome table after running the query,
-I want to give a name to the subtotal and grandtotal using the GROUPING() function.
-*/
-
-SELECT 
+# roll up by row type
+	
+	SELECT 
     customer_id,
     aircraft_id,
     SUM(Price_per_ticket) AS total_price_all_tickets,
@@ -94,16 +94,26 @@ FROM
 GROUP BY 
     customer_id, aircraft_id WITH ROLLUP;
 
-# WAQ to create a view with only business class customers along with the brand of airlines.
+
+## Task 11: WAQ to calculate the total price of all tickets booked by a customer across different aircraft IDs using rollup function.
+
+select customer_id, aircraft_id, sum(Price_per_ticket) as tot_price_all_tickets
+from ticket_details
+group by customer_id, aircraft_id 
+with rollup ;
+
+
+## Task 12: WAQ to create a view with only business class customers along with the brand of airlines.
 
 CREATE  OR REPLACE VIEW business_class_customers AS
 SELECT customer_id, class_id, brand
 FROM ticket_details
 WHERE class_id ='Business';
 
-# WAQ to create a stored procedure to get the details of all passengers flying between a range of routes defined in run time. 
-	# also, return an error message if the table doesn't exist.
 
+## Task 13: WAQ to create a stored procedure to get the details of all passengers flying between a range of routes defined in run time. 
+	# also, return an error message if the table doesn't exist.
+'
 DROP procedure IF EXISTS `get_passengers_by_route`;
 
 DELIMITER //
@@ -128,7 +138,7 @@ DELIMITER ;
 
 CALL get_passengers_by_route('CRW', 'COD');
 
-# WAQ to create a stored procedure that extract all from the routes table where the travelled distance is more than 2000 miles. 
+## Task 14: WAQ to create a stored procedure that extract all from the routes table where the travelled distance is more than 2000 miles. 
 DELIMITER //
 
 CREATE PROCEDURE get_long_distance_routes()
@@ -144,7 +154,7 @@ DELIMITER ;
 CALL get_long_distance_routes();
 
 
-# WAQ to create a stored procedure that groups the distance travelled by each flight into three categories
+## Task 15: WAQ to create a stored procedure that groups the distance travelled by each flight into three categories
 /* The categories are:
 - short distance travel (SDT) for >=0 AND <= 2000 miles, 
 - intermediate distance travel (IDT) for >2000 AND <=6500, 
@@ -167,7 +177,7 @@ END$$
 DELIMITER ;
 CALL travel_distance(2500);
 
-# WAQ to extract ticket purchase date, customer id, class id, 
+## Task 16: WAQ to extract ticket purchase date, customer id, class id, 
 	# and specify if the complimentary services are provided for the specific class 
 	# using a stored function in stored procedure on the ticket_details table.
 /* Condition:
@@ -212,7 +222,7 @@ CALL get_ticket_services();
 
 DELIMITER //
 
-# WAQ to extract the first record of the customer whose last name ends with Scott using a cursor from the customer table.
+## Task 17: WAQ to extract the first record of the customer whose last name ends with Scott using a cursor from the customer table.
 
 CREATE PROCEDURE get_first_scott_customer()
 BEGIN
